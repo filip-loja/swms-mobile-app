f0ee8481b3abddffa012694072878e8c261e5070
Filip Loja <filiploja@gmail.com>
2021-05-05 12:07:58 +0200

    Create About modal

---
+++ src/components/ModalAbout.vue
@@ -0,0 +1,56 @@
+<template>
+	<ion-header>
+		<ion-toolbar>
+			<ion-title>About</ion-title>
+			<ion-buttons slot="end">
+				<ion-button expand="block" @click="closeModal">
+					<ion-icon slot="icon-only" :icon="closeOutline" />
+				</ion-button>
+			</ion-buttons>
+		</ion-toolbar>
+	</ion-header>
+	<ion-content class="ion-padding">
+
+		<ion-card class="ion-text-center">
+			<ion-card-header>
+				<br />
+				<ion-row class="ion-justify-content-center">
+					<ion-avatar>
+						<img src="lnu_logo.jpg">
+					</ion-avatar>
+				</ion-row>
+				<br />
+				<ion-card-subtitle>Created by: <b>Filip Loja</b></ion-card-subtitle>
+				<ion-card-title>Smart Waste Management System</ion-card-title>
+			</ion-card-header>
+
+			<ion-card-content>
+				This application was developed as a part of the final assignment in Internet Architectures course at Linnaeus University in 2021.
+			</ion-card-content>
+			<br />
+		</ion-card>
+
+	</ion-content>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, modalController, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonAvatar, IonRow } from '@ionic/vue'
+import { defineComponent } from 'vue'
+import { closeOutline } from 'ionicons/icons'
+
+export default defineComponent({
+	name: 'Modal',
+	components: { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonAvatar, IonRow },
+	setup () {
+
+		const closeModal = async () => {
+			await modalController.dismiss()
+		}
+
+		return {
+			closeOutline,
+			closeModal
+		}
+	}
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>SWMS App</ion-title>
+				<ion-buttons slot="end">
+					<ion-button expand="block" @click="showModalAbout">
+						<ion-icon slot="icon-only" :icon="helpCircleOutline" />
+					</ion-button>
+				</ion-buttons>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
@@ -22,8 +27,10 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, modalController  } from '@ionic/vue'
+import { defineComponent } from 'vue'
+import { helpCircleOutline } from 'ionicons/icons'
+import ModalAbout from '@/components/ModalAbout.vue'
 
 export default defineComponent({
   name: 'Home',
@@ -32,15 +39,33 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
-});
+    IonToolbar,
+		IonButtons,
+		IonButton,
+		IonIcon
+  },
+	setup () {
+
+		const showModalAbout = async () => {
+			const modal = await modalController
+				.create({
+					component: ModalAbout
+				})
+			return modal.present()
+		}
+
+		return {
+			helpCircleOutline,
+			showModalAbout
+		}
+	}
+})
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +81,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
